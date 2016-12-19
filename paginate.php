d1937216f24e6d273f2fcf276d7e86339918933d
Khaled Fares <khaledfares@Khaleds-MacBook-Pro.local>
2016-12-19 03:53:32 +0200

    splittig the helper from the paginate.php file

--- paginate.php
+++ paginate.php
@@ -1,27 +1,6 @@
 <?php
-require 'Article.php';
+require 'helper.php';
 $connection =  require 'connection.php';
-
-function query($connection, $offset, $articlesPerPage)
-{
-
-	$selectAllArticles  = $connection->prepare("SELECT * FROM articles LIMIT $offset, $articlesPerPage");
-	$selectAllArticles->execute();
-  return  $selectAllArticles->fetchAll( PDO::FETCH_CLASS, 'Article' );
-
-}
-
-
-function numberOfRecords($connection){
-	$selectAllArticles  = $connection->prepare("SELECT COUNT(*) FROM articles");
-	$selectAllArticles->execute();
-	return (int) $selectAllArticles->fetchAll()[0][0];
-}
-
-function positiveInt($string)
-{
-	return  (int) $string   >  0 ? (int) $string : 1;
-}
 
 function paginate($connection, $pages, $articlesPerPage, $maxPerPage  = 5)
 {
@@ -36,18 +15,6 @@ function paginate($connection, $pages, $articlesPerPage, $maxPerPage  = 5)
 		"articles"=> $articles
 	];
 }
-
-function limitPages($connection, $pages, $articlesPerPage)
-{
-	$lastPagePossible =  ceil(numberOfRecords($connection) / $articlesPerPage);
-	return $lastPagePossible < $pages  ? $lastPagePossible : $pages;
-}
-
-function limitArticles($connection, $articlesPerPage, $maxPerPage)
-{
-	return 	$articlesPerPage > numberOfRecords($connection) ? $maxPerPage : $articlesPerPage;
-}
-
 
 return  paginate(
 	$connection,
